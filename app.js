edc9d67b1dee9050f61e8c335a8be62b67d7a435
unknown <adhirsoechit@gmail.com>
2021-02-04 09:36:30 -0400

    Checking parallax effect

--- app.js
+++ app.js
@@ -4,9 +4,34 @@ const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
 toggle.addEventListener("click",function(){
   navbarLinks.classList.toggle("active");
+  return false;
 });
 
 
+// INTERSECTION OBSERVER
+
+const header = document.querySelector(".navbar");
+const sectionOne = document.querySelector(".church-image");
+
+const sectionOneOptions = {
+  rootMargin: "-15% 0%"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("navbar-intersecting");
+    } else {
+      header.classList.remove("navbar-intersecting");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
 
 // EMAILJS FORM
 
@@ -37,8 +62,7 @@ $( "#remainingC" ).html( "Remaining characters: 200");
 
 
 
-
-
+const errorElement = document.getElementById('error');
 
 function sendMail(params){
   var templateParams = {
@@ -47,80 +71,51 @@ function sendMail(params){
 message:document.getElementById("msg").value
   };
 
+
+
   emailjs.send('service_lmz8mb8', 'template_r3cpqa5', templateParams)
       .then(function(response) {
          console.log('SUCCESS!', response.status, response.text);
       }, function(error) {
          console.log('FAILED...', error);
+
       });
 
-// document.getElementById('fromName').value=null;
-// document.getElementById('toName').value=null;
-// document.getElementById('msg').value=null;
-// $( "#remainingC" ).html( "Remaining characters: 200");
+
+      document.getElementById('fromName').value=null;
+      document.getElementById('toName').value=null;
+      document.getElementById('msg').value=null;
+      $( "#remainingC" ).html( "Remaining characters: 200");
+      return false;
+
+
 }
 
-// const name = document.getElementById("name");
-// const form = document.getElementById("form");
+const button = document.querySelector(".bttn");
+const modalBg = document.querySelector(".modal-bg");
+const modalClose = document.querySelector(".modal-close");
 
-// onclick="sendMail()"
-//
-//  const toname = document.getElementById("toName").value;
-// const errorElement = document.getElementById('error');
-// const form = document.getElementById('form');
-// form.addEventListener("submit",(e) => {
-//   if (to_name ===''|| to_name === null){
-//     e.preventDefault();
-//     console.log("This was a failure");
-//   }
-//
-//   else{
-//     console.log("Success!");
-//   }
-// });
+button.addEventListener("click",function(){
+  modalBg.classList.add("bg-active");
+  disableScroll();
+});
 
+modalClose.addEventListener("click",function(){
+  modalBg.classList.remove("bg-active");
+  enableScroll();
+});
 
-// form.addEventListener("submit", (e) =>{
-// let messages=[];
-// if(toname === ''){
-// messages.push('Name is required for this field');
-// }
-//   if(messages.length >0){
-//     e.preventDefault();
-//     errorElement.innerText = messages.join(',');
-//   }
-// });
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
 
-// const name = document.getElementById('name');
-// const password = document.getElementById('password');
-// const form = document.getElementById('form');
-// const errorElement = document.getElementById('error');
-//
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
 
-
-// function myFormHandler(){
-//   form.addEventListener('submit', (e) => {
-//     let messages = [];
-//
-//     if (name.value === '' || name.value == null) {
-//       messages.push('Name is required');
-//     }
-//
-//     if (password.value.length <= 6) {
-//       messages.push('Password must be longer than 6 characters');
-//     }
-//
-//     if (password.value.length >= 20) {
-//       messages.push('Password must be less than 20 characters');
-//     }
-//
-//     if (password.value === 'password') {
-//       messages.push('Password cannot be password');
-//     }
-//
-//     if (messages.length > 0) {
-//       e.preventDefault();
-//       errorElement.innerText = messages.join(', ');
-//     }
-//   });
-// }
+function enableScroll() {
+    window.onscroll = function() {};
+}

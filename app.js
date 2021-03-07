f6502b16396455374a3eade513ca2b6111cf601c
polymer-coder <developer.polymer@gmail.com>
2021-03-07 10:28:56 -0400

    refactored app.js to use guard clause

--- app.js
+++ app.js
@@ -2,33 +2,36 @@
 const toggle = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
-toggle.addEventListener("click",function(){
+toggle.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
   return false;
 });
 
+changeNavColor();
 
-// INTERSECTION OBSERVER
+function changeNavColor() {
+  // INTERSECTION OBSERVER
 
-const header = document.querySelector(".navbar");
-const sectionOne = document.querySelector(".church-image");
+  const header = document.querySelector(".navbar");
+  const sectionOne = document.querySelector(".church-image");
 
-const sectionOneOptions = {
-  rootMargin: "-15% 0%"
-};
+  if (!sectionOne) return; //if the church image doesn't exist, exit function
 
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("navbar-intersecting");
-    } else {
-      header.classList.remove("navbar-intersecting");
-    }
-  });
-},
-sectionOneOptions);
+  const sectionOneOptions = {
+    rootMargin: "-15% 0%",
+  };
 
-sectionOneObserver.observe(sectionOne);
+  const sectionOneObserver = new IntersectionObserver(
+    (entries, sectionOneObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          header.classList.add("navbar-intersecting");
+        } else {
+          header.classList.remove("navbar-intersecting");
+        }
+      });
+    },
+    sectionOneOptions
+  );
+  sectionOneObserver.observe(sectionOne);
+}

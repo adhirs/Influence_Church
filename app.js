18c82fdd17e5ff628b00aaee70e98a6f11b4d09a
polymer-coder <developer.polymer@gmail.com>
2021-03-09 16:12:39 -0400

    dynamically loaded iframe

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ toggle.addEventListener("click", () => {
 });
 
 changeNavColor();
+loadPlugins();
+
+function loadPlugins() {
+  if ('loading' in HTMLIFrameElement.prototype) {
+    const ytIframe = document.querySelector('#ytIframe');
+    const fbIframe = document.querySelector('#fbIframe');
+    if(!ytIframe && !fbIframe) return; //neither elements are present, bail out
+    ytIframe.src = "http://www.youtube.com/embed/videoseries?list=UUBZKuZV7UrYa2NOlRxP5MEg&origin=https://influencechurchtt.org"
+    fbIframe.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Finfluencechurch.tt&tabs=timeline%2Cevents&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+  }
+}
 
 function changeNavColor() {
   // INTERSECTION OBSERVER
